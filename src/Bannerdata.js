90b3f667bad4d7d105b18d56dfdbffdb5d49559d
Deepak Yadav <98831114+deepak1091@users.noreply.github.com>
2024-03-27 19:13:38 +0530

    Update Bannerdata.js
    text change

--- src/Bannerdata.js
+++ src/Bannerdata.js
@@ -2,7 +2,7 @@ const Bannerdata = [
     {
       bannerid : 1,
       bannertitle : "Deepak Yadav",
-      bannersubtitle : "Lead Designer (Sr. UI/UX Designer) " ,
+      bannersubtitle : "Lead Designer (Sr. UI/UX Designer)" ,
       bannerbutton : "Contact Us",
       bannerimage : "../images/profile.png"
     },
